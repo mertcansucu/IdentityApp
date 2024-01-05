40f2cf91c6d5675258369d67a7b5d45d3b6b4b49
mertcansucu <mrtcnscc@gmail.com>
2024-01-05 21:28:07 +0300

    adding user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace IdentityApp.Controllers
 {
@@ -17,6 +19,36 @@ namespace IdentityApp.Controllers
         }
         public IActionResult Index(){
             return View(_userManager.Users);//user bilgilerini getirip view içine gönderdim
+        }
+
+        public IActionResult Create(){
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model){
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser{UserName = model.UserName, Email = model.Email};
+
+                IdentityResult result = await _userManager.CreateAsync(user,model.Password);
+                //await _userManager.CreateAsync(user); şifresizde kayıt yapabilirim ama kullancı girişi olduğu için uygulamamda böyle yaptım
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("",err.Description);
+                }
+
+                /*
+                   mrtcnsc@gmail.com - Mert19071.                 
+                */
+            }
+            return View(model);
         }
     }
 }
