e73cd7cd2a6622b6be90743a07fa4c12b0215df6
mertcansucu <mrtcnscc@gmail.com>
2024-01-08 05:31:36 +0300

    Assigning Roles to Users

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -122,6 +122,12 @@ namespace IdentityApp.Controllers
                     
                     if (result.Succeeded)
                     {
+                        await _userManager.RemoveFromRolesAsync(user, await _userManager.GetRolesAsync(user));//ben her editleme işleminde o kullanıcıya ait rol bilgilerini başta sildim
+
+                        if (model.SelectedRoles != null)//eğer ben editte bir rol seçtiysem ekleme yapıcam
+                        {
+                            await _userManager.AddToRolesAsync(user, model.SelectedRoles);
+                        }
                         return RedirectToAction("Index");
                     }
 
